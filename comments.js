0ed9d70d168e9d1d205875f7cb3d8c6fbec39bf5
Aryan Bhardwaj <100504332+Aryan8445@users.noreply.github.com>
2024-03-16 06:05:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,9 +1,63 @@
-//create web server
+// create web server
+// create a file comments.js
+// 1. create a web server
+// 2. create a file comments.js
+// 3. create a page that shows all the comments
+// 4. create a page that adds a comment
+// 5. create a page that deletes a comment
+// 6. create a page that updates a comment
+
 var http = require('http');
-var server = http.createServer(function(req, res) {
-  // Set the response status code to 200 (OK)
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  // Send a response body
-  res.end('Hello World\n');
-});
-// Listen on port 3000, IP defaults to
+var fs = require('fs');
+var url = require('url');
+var querystring = require('querystring');
+var comments = [];
+
+var server = http.createServer(function (req, res) {
+    var url_parts = url.parse(req.url);
+    var query = querystring.parse(url_parts.query);
+
+    if (url_parts.pathname == '/add') {
+        if (query.comment) {
+            comments.push(query.comment);
+        }
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end('<a href="/comments">Comments</a>');
+    } else if (url_parts.pathname == '/delete') {
+        if (query.id) {
+            comments.splice(query.id, 1);
+        }
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end('<a href="/comments">Comments</a>');
+    } else if (url_parts.pathname == '/update') {
+        if (query.id && query.comment) {
+            comments[query.id] = query.comment;
+        }
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end('<a href="/comments">Comments</a>');
+    } else if (url_parts.pathname == '/comments') {
+        var html = '<!DOCTYPE html>' +
+            '<html>' +
+            '<head>' +
+            '<title>Comments</title>' +
+            '</head>' +
+            '<body>';
+
+        for (var i in comments) {
+            html += '<p>' + comments[i] + ' <a href="/update?id=' + i + '">Update</a> <a href="/delete?id=' + i + '">Delete</a></p>';
+        }
+
+        html += '<form action="/add" method="get">' +
+            '<input type="text" name="comment">' +
+            '<input type="submit" value="Add Comment">' +
+            '</form>' +
+            '</body>' +
+            '</html>';
+
+
